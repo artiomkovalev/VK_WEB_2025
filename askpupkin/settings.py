28628a268e156fe4ab636fffcd47aae1b2389880
artiomkovalev <artiomkratov@gmail.com>
2025-11-16 21:54:01 +0300

    fix: fixed known issues

--- askpupkin/settings.py
+++ askpupkin/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-kn5+6e*yawv&_4gf1ziz@#xui7%%8)tap1$bj21$%ampwm0c_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
